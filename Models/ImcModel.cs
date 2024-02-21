096ee6c2ed503a22844c35a7546b0e224b74c8d3
Gabriel Lakatos <gabriellakatos@gmail.com>
2024-02-21 07:34:54 -0300

    Finalizado o PWA

---
+++ Models/ImcModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Imc.Models
+{
+    public class ImcModel
+    {
+        [Required(ErrorMessage="Informe a altura")]
+        [Range(1,3, ErrorMessage ="Altura inválida")]
+        public double? Height {get; set; }
+
+        [Required(ErrorMessage = "Informe o peso")]
+        public double? Weight { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        // As duas interrogações (??) após as propriedades indicam que o valor default será '1' caso a propriedade venha nula
+        public double Imc => (Weight ?? 1) / ((Height ?? 1) * ( Height ?? 1));
+    }
+}
